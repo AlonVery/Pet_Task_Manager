7e7c068100f6f912b6eee54a6664124c17420c99
Stepanov Alexey <stepka.games@mail.ru>
2026-02-10 01:49:59 +0500

    del

--- docker/postgres/init/001_types.sql
+++
@@ -1,8 +0,0 @@
--- auto-generated definition
-create type user_status as enum ('ACTIVE', 'INACTIVE', 'PENDING', 'BLOCKED', 'DELETED', 'SUSPENDED');
-alter type user_status owner to postgres;
-
--- auto-generated definition
-create type user_role as enum ('USER', 'ADMIN', 'MANAGER');
-alter type user_role owner to postgres;
-
--- docker/postgres/init/003_indexes.sql
+++
@@ -1,2 +0,0 @@
-create unique index uniq_index_username_email
-    on users_for_test ((lower(user_name)), email);
